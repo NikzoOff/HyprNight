e871527ea392fa4ecbbd915ff77013b1a8a3293c
NikzoOff <joane.seguin23@gmail.com>
2025-10-26 21:12:50 +0100

    Added Hypr/Fish/fastfetch/nvim configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+starship init fish | source
+
+# ALIASES
+alias i="yay -S"
+alias iyu="yay -Syu"
+alias iy="yay -Sy"
+alias nv="nvim"
+alias iss="yay -Ss"
+alias ins="yay -Rns"
+
+fastfetch
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
